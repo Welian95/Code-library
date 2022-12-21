9e49f9c2f2e8d2ed2d2b7fcef0c3c7a28448383d
Julian Westerhorstmann <jwesterhorstmann@bode.ms>
2022-12-21 13:52:47 +0100

    add streamlit for testing2

---
+++ streamlit_app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+### Windows Installing:
+# Erstelle eine Virtuelle Umgebung mit Anaconda
+#pip install streamlit
+# testen im terminal mit :  
+    # streamlit hello
+# https://github.com/streamlit/streamlit/wiki/Installing-in-a-virtual-environment
+
+### Run App
+
+#Start the App with terminal command: 
+    #streamlit run app.py
+
+#Achtung Streamlit muss in der Virtuelen Umgebung ausgeführt werden in der auch die app.py liegt!
+
+st.title('Hello Julian__hh') #create the Titel of the page
+
+st.text('This is a streamlit app to run all of Julians code') #Text on page 
+st.markdown (' This is **markdown**') #Markdown language with operators like '###' fpr headings ans '**' for bold 
